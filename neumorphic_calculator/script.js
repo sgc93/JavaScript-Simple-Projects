1fa5e70876bfa71737dd21276e6c1e11a9e5851c
sgc93 <smachewgedefaw@gmail.com>
2023-10-14 15:42:50 -0900

    handle keys clicking

--- neumorphic_calculator/script.js
+++ neumorphic_calculator/script.js
@@ -1,6 +1,50 @@
 const input_field = document.querySelector(".input");
 const clear = document.querySelector(".clear");
 const negate = document.querySelector(".negate");
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const zero = document.querySelector(".zero");
+const zero_zero = document.querySelector(".zero-zero");
+const modulo = document.querySelector(".module");
+const divide = document.querySelector(".divide");
+const times = document.querySelector(".times");
+const minus = document.querySelector(".minus");
+const plus = document.querySelector(".plus");
+const equal = document.querySelector(".equal");
+const dot = document.querySelector(".dot");
+
+const keys = [
+	one,
+	two,
+	three,
+	four,
+	five,
+	six,
+	seven,
+	eight,
+	nine,
+	zero,
+	zero_zero,
+	modulo,
+	divide,
+	times,
+	minus,
+	plus,
+	dot,
+];
+
+keys.forEach((key) => {
+	key.addEventListener("click", () => {
+		input_field.value += key.innerHTML;
+	});
+});
 
 clear.addEventListener("click", clearField);
 negate.addEventListener("click", negateValue);

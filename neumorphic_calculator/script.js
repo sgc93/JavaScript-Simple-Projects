81ba9c5daa169e94cfc1aa4612ae47fafcdfca79
sgc93 <smachewgedefaw@gmail.com>
2023-10-14 15:20:02 -0900

    handle negation

--- neumorphic_calculator/script.js
+++ neumorphic_calculator/script.js
@@ -1,8 +1,14 @@
 const input_field = document.querySelector(".input");
 const clear = document.querySelector(".clear");
+const negate = document.querySelector(".negate");
 
 clear.addEventListener("click", clearField);
+negate.addEventListener("click", negateValue);
 
 function clearField() {
 	input_field.value = 0;
+}
+
+function negateValue() {
+	input_field.value = -input_field.value;
 }

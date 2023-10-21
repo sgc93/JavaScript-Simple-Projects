09e604e0f2e4291fdb1b1838ed9992feaa66e775
sgc93 <smachewgedefaw@gmail.com>
2023-10-21 08:19:54 -0900

    add classes to add dynamically

---
+++ whereAmI/script.js
@@ -0,0 +1,8 @@
+"strict mode";
+
+/////
+
+const placeSpan = document.querySelector(".place");
+const moreP = document.querySelector(".more");
+const askBtn = document.querySelector(".ask-btn");
+const moreBtn = document.querySelector("more-btn");

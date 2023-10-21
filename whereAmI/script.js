aa4e011232417ee9dbaccfc2b6298bfeb433253e
sgc93 <smachewgedefaw@gmail.com>
2023-10-21 08:58:20 -0900

    handling position asking button

--- whereAmI/script.js
+++ whereAmI/script.js
@@ -6,3 +6,15 @@ const placeSpan = document.querySelector(".place");
 const moreP = document.querySelector(".more");
 const askBtn = document.querySelector(".ask-btn");
 const moreBtn = document.querySelector("more-btn");
+
+const getLocation = function () {
+	return new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(resolve, reject);
+	});
+};
+
+const whereAmI = getLocation()
+	.then((position) => console.log(position))
+	.catch((err) => console.log(err));
+
+askBtn.addEventListener("click", whereAmI);

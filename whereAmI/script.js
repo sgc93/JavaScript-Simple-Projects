c456588c37abbb2411a4d89634076761ef8c996c
sgc93 <smachewgedefaw@gmail.com>
2023-10-21 10:14:18 -0900

    display location with function

--- whereAmI/script.js
+++ whereAmI/script.js
@@ -9,24 +9,42 @@ const moreBtn = document.querySelector("more-btn");
 
 const getLocationCordinates = function () {
 	return new Promise((resolve, reject) => {
-		navigator.geolocation.getCurrentPosition(resolve, reject);
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(resolve, reject);
+		} else {
+			throw new Error("The browser does not support geolocation.");
+		}
 	});
 };
 
+const displayResult = (data, type) => {
+	if (type === "error") {
+		placeSpan.innerHTML = "...";
+	} else {
+		placeSpan.innerHTML = data;
+	}
+};
+
 const getLocationName = function (latitude, longitude) {
-	fetch(`https://geocode.xyz/${latitude},${longitude}?geoit=json`)
+	fetch(`https://geocode.xyz/${latitude},${longitude}?json=1`)
 		.then((response) => {
 			if (!response.ok) {
-				throw new Error("error happend");
+				throw new Error("Network response was not ok.");
 			}
 			return response.json();
 		})
 		.then((parsedData) => {
+			console.log(`API Error: ${parsedData.error}.`);
+			if (parsedData.error) {
+				throw new Error(`API Error: ${parsedData.error}.`);
+			}
 			const data = [parsedData][0];
-			console.log(data);
-			placeSpan.innerHTML = `${data.city},${data.country}`;
+			displayResult(data.city, "data");
 		})
-		.catch((err) => console.log(err));
+		.catch((err) => {
+			displayResult(err, "error");
+			console.log(err);
+		});
 };
 
 function whereAmI() {

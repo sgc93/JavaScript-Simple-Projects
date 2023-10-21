fe0a0bff16c8df40f5332035a8f86bb40586192a
sgc93 <smachewgedefaw@gmail.com>
2023-10-21 09:21:42 -0900

    fetching location name from geocode.xyz

--- whereAmI/script.js
+++ whereAmI/script.js
@@ -7,14 +7,36 @@ const moreP = document.querySelector(".more");
 const askBtn = document.querySelector(".ask-btn");
 const moreBtn = document.querySelector("more-btn");
 
-const getLocation = function () {
+const getLocationCordinates = function () {
 	return new Promise((resolve, reject) => {
 		navigator.geolocation.getCurrentPosition(resolve, reject);
 	});
 };
 
-const whereAmI = getLocation()
-	.then((position) => console.log(position))
-	.catch((err) => console.log(err));
+const getLocationName = function (latitude, longitude) {
+	fetch(`https://geocode.xyz/${latitude},${longitude}?geoit=json`)
+		.then((response) => response.json())
+		.then((parsedData) => {
+			const data = [parsedData][0];
+			const city = data.city;
+			const country = data.country;
+		})
+		.catch((err) => console.error(err));
+};
+
+function whereAmI() {
+	getLocationCordinates()
+		.then((position) => {
+			const latitude = position.coords.latitude;
+			const longitude = position.coords.longitude;
+			console.log(position.coords);
+			console.log(
+				`coordinates:- lat = ${position.coords.latitude}, long = ${position.coords.longitude}`
+			);
+			getLocationName(latitude, longitude);
+		})
+		.catch((err) => console.log(err));
+	console.log("fetching data ...");
+}
 
 askBtn.addEventListener("click", whereAmI);
